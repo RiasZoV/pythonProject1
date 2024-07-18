6a0c39bf903a3c7ec95f53dafa6a1249de3880bb
Rias <vitja2603@rambler.ru>
2024-07-18 16:12:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from database import Role, User, Function
 
 
 def admin_actions(user):
+    """
+    Действия для администратора.
+
+    Параметры:
+    - user (User): Объект текущего пользователя.
+    """
     actions = {
         1: "view_profile",
         2: "add",
@@ -24,9 +30,9 @@ def admin_actions(user):
         for num, action in actions.items():
             print(f"{num} - {action}")
 
-        action = input("Введите номер действия: ").strip()
-        if action.isdigit():
-            action_num = int(action)
+        action_num = int(input("Введите номер действия: ").strip())
+        action = actions.get(action_num)
+
         if action == "view_profile":
             view_profile(user)
         elif action == "add":
@@ -41,11 +47,11 @@ def admin_actions(user):
             age = int(input("Введите возраст нового пользователя: ").strip())
             add_user(new_login, new_password, new_role.name, age)
         elif action == "delete":
-            user_login = input("Введите login пользователя для удаления: ").strip()
+            user_login = input("Введите логин пользователя для удаления: ").strip()
             user_to_delete = get_user_by_login(user_login)
             delete_user(user_to_delete.id)
         elif action == "change_role":
-            user_login = input("Введите login пользователя для изменения роли: ").strip()
+            user_login = input("Введите логин пользователя для изменения роли: ").strip()
             user_to_change = get_user_by_login(user_login)
             roles = list_roles()
             print("Доступные роли:")
@@ -55,14 +61,14 @@ def admin_actions(user):
             new_role = get_role_by_number(role_num)
             change_user_role(user_to_change.id, new_role.name)
         elif action == "list":
-            list_users()
+            print(list_users())
         elif action == "change_password":
-            user_login = input("Введите login пользователя для смены пароля: ").strip()
+            user_login = input("Введите логин пользователя для смены пароля: ").strip()
             user_to_change = get_user_by_login(user_login)
             new_password = input("Введите новый пароль: ").strip()
             change_password(user_to_change.id, new_password)
         elif action == "change_subordinates":
-            user_login = input("Введите login пользователя для изменения подчиненных: ").strip()
+            user_login = input("Введите логин пользователя для изменения подчиненных: ").strip()
             user_to_change = get_user_by_login(user_login)
             subordinates_logins = input("Введите логины новых подчиненных через запятую: ").strip().split(',')
             change_subordinates(user_to_change.id, [login.strip() for login in subordinates_logins])
@@ -75,6 +81,12 @@ def admin_actions(user):
 
 
 def manager_actions(user):
+    """
+    Действия для менеджера.
+
+    Параметры:
+    - user (User): Объект текущего пользователя.
+    """
     actions = {
         1: "view_profile",
         2: "list_subordinates",
@@ -93,9 +105,10 @@ def manager_actions(user):
         if action == "view_profile":
             view_profile(user)
         elif action == "list_subordinates":
-            list_subordinates(user.id)
+            subordinates = list_subordinates(user.id)
+            print(subordinates)
         elif action == "change_password":
-            subordinate_login = input("Введите login подчиненного для смены пароля: ").strip()
+            subordinate_login = input("Введите логин подчиненного для смены пароля: ").strip()
             subordinate = get_user_by_login(subordinate_login)
             new_password = input("Введите новый пароль: ").strip()
             change_password(subordinate.id, new_password)
@@ -108,6 +121,12 @@ def manager_actions(user):
 
 
 def user_actions(user):
+    """
+    Действия для обычного пользователя.
+
+    Параметры:
+    - user (User): Объект текущего пользователя.
+    """
     actions = {
         1: "view_profile",
         2: "change_password",
@@ -137,12 +156,20 @@ def user_actions(user):
 
 
 def view_profile(user):
-    """Просмотр профиля пользователя"""
+    """
+    Просмотр профиля пользователя.
+
+    Параметры:
+    - user (User): Объект текущего пользователя.
+    """
     print(f"Профиль пользователя:\nЛогин: {user.login}\nВозраст: {user.age}")
 
 
 def initialize_database():
-    """Добавление ролей и функций только при первом запуске"""
+    """
+    Инициализация базы данных.
+    Добавление ролей и функций только при первом запуске.
+    """
     session = get_session()
 
     # Проверка, существуют ли уже роли и функции
@@ -160,7 +187,9 @@ def initialize_database():
 
 
 def add_initial_users():
-    """Добавление начальных пользователей только при первом запуске"""
+    """
+    Добавление начальных пользователей только при первом запуске.
+    """
     session = get_session()
     if not session.query(User).first():
         print("Заполнение базы данных начальными пользователями...")
